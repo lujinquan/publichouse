c2ef80d11a9955067ed897f68bbb0eeaaaa42cda
Lucas <18674012767@163.com>
2018-08-03 11:41:56 +0800

    sd

---
+++ application/ph/validate/ConfirmHouseInfo.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace app\ph\validate;
+
+use think\Validate;
+
+/**
+ * 租户验证器
+ * @package app\ph\validate
+ * @author Mr.Lu
+ */
+class ConfirmHouseInfo extends Validate
+{
+    protected $rule = [
+
+        'UseNature|使用性质'  => 'require',
+        'UnitID|单元号'  => 'require',
+        'FloorID|楼层号'  => 'require',
+
+    ];
+
+    protected $message = [
+//        'HouseID.require' => '房屋编号不能为空',
+//        'HouseID.length' => '房屋编号必须为10位数长度',
+    ];
+
+    //定义验证场景
+    protected $scene = [
+
+    ];
+}
