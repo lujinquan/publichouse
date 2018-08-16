ad1a444f97ebd9c32a7d15485428870b6995130b
TD-winter <747386743@qq.com>
2018-08-16 15:17:25 +0800

    租约

--- public/static/gf/viewJs/lease_audit.js
+++ public/static/gf/viewJs/lease_audit.js
@@ -1,5 +1,7 @@
 $('.print2').click(function(event){
 	event.stopPropagation();
+	var houseID = $(this).val();
+	console.log(houseID);
 	layer.open({
 		type:1,
 		area:['750px','700px'],
@@ -7,12 +9,22 @@ $('.print2').click(function(event){
 		zIndex:100,
 		title:['租约申请','color:#FFF;font-size:1.6rem;font-weight:600;'],
 		content:$('#print2'),
-		btn:['确认','取消'],
+		btn:['打印','取消'],
 		success:function(){
-
+			$.get('/ph/LeaseAudit/detail/ChangeOrderID/'+houseID,function(res){
+				var res = JSON.parse(res);
+				
+				var data = res.data.detail;
+				console.log(data);
+				for(var key in data){
+					var name_id = key.replace(/apply/,'printer_2_');
+					$('#'+name_id).text(data[key]);
+				}
+			})
 		},
 		yes:function(){
 
 		}
 	})
-})
+});
+
