5020135d258b3e3ee666560e0f27c2dec389293e
TD-winter <747386743@qq.com>
2018-09-13 14:43:11 +0800

    身份证图片压缩上传

--- public/static/gf/viewJs/tenant_form.js
+++ public/static/gf/viewJs/tenant_form.js
@@ -26,8 +26,12 @@ $("#addTenant").click(function(){
 			content:$('#TenantForm'),
 			btn:['确定','取消'],
 			yes:function(thisIndex){
+				$('#imgUp').prop('name','');
+				$('#imgDown').prop('name','');
+
 				var data = new FormData($('#TenantForm')[0]);
-				console.log(data);
+				data.append('IDCardFace',dataURLtoFile($('#IDCardFace').prop('src'),'IDCardFace'));
+				data.append('IDCardReverse',dataURLtoFile($('#IDCardReverse').prop('src'),'IDCardReverse'));
 				if(flag==true){
 					flag =false;
 					$.ajax({
@@ -316,3 +320,66 @@ function readFile(fileUp,fileShow){
 // $('#doc-vld-email-5,#BanI').blur(function(){
 // 	Validation.bankCard($(this).val());
 // });
+// 
+
+$(function (){
+    $("#imgUp").change(function () {
+        var file = $(this)[0].files[0];//获取input file控件选择的文件
+        ImgToBase64(file, 720, function (base64) {
+            $("#IDCardFace")[0].src = base64;//预览页面上预留一个img元素，载入base64
+            //$("#IDCardFace")[0].width = 300;//设定宽高，不然会自动按照压缩过的图片宽高设定，有可能超出预想的范围。
+        });
+    });
+
+    $("#imgDown").change(function () {
+        var file = $(this)[0].files[0];//获取input file控件选择的文件
+        ImgToBase64(file, 720, function (base64) {
+            $("#IDCardReverse")[0].src = base64;//预览页面上预留一个img元素，载入base64
+            //$("#IDCardFace")[0].width = 300;//设定宽高，不然会自动按照压缩过的图片宽高设定，有可能超出预想的范围。
+        });
+    })
+})
+
+function ImgToBase64(file, maxLen, callBack) {
+    var img = new Image();
+    var reader = new FileReader();//读取客户端上的文件
+    reader.onload = function () {
+        var url = reader.result;//读取到的文件内容.这个属性只在读取操作完成之后才有效,并且数据的格式取决于读取操作是由哪个方法发起的.所以必须使用reader.onload，
+        img.src = url;//reader读取的文件内容是base64,利用这个url就能实现上传前预览图片
+    };
+    img.onload = function () {
+        //生成比例
+        var width = img.width, height = img.height;
+        //计算缩放比例
+        var rate = 1;
+        if (width >= height) {
+            if (width > maxLen) {
+                rate = maxLen / width;
+            }
+        } else {
+            if (height > maxLen) {
+                rate = maxLen / height;
+            }
+        };
+        img.width = width * rate;
+        img.height = height * rate;
+        //生成canvas
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        var base64 = canvas.toDataURL('image/jpeg', 0.9);
+        callBack(base64);
+    };
+    reader.readAsDataURL(file);
+}
+
+function dataURLtoFile(dataurl, filename) {//base64转换成文件
+	var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+	bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+	while(n--){
+	  u8arr[n] = bstr.charCodeAt(n);
+	}
+	return new File(u8arr, filename, {type:mime});
+}
