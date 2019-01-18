8b707c7350db35c2833d10185b9bdc5abff1d854
Lucas <18674012767@163.com>
2019-01-18 15:57:19 +0800

    上面的计租表搬到下面来

--- application/ph/controller/ConfirmHouseInfo.php
+++ application/ph/controller/ConfirmHouseInfo.php
@@ -224,6 +224,201 @@ class ConfirmHouseInfo extends Base
                         'RoomName' => $one['RoomTypeName'],
                         'RentPointIDS' => $rentPointIDS,
                     ];
+//halt($datas);
+                    if ($datas['RoomID']) {  //如果房间编号存在，则为修改
+                        //halt($datas['RoomID']);
+                        $datas['UpdateTime'] = time();
+                        
+                        if ($v5[14] == 1) { //删除中状态
+                            Db::name('room')->where('RoomID', $v5[0])->delete();
+                        }else{
+                            //修改，或者删除，要回滚面积
+                            $findOne = Db::name('room')->field('HouseID,BanID,UseArea,LeasedArea,RoomPublicStatus')->where('RoomID', $v5[0])->find();
+                            if ($findOne['RoomPublicStatus'] == 1) {    //私有房屋
+                                Db::name('house')->where('HouseID', 'eq', $findOne['HouseID'])->setDec('HouseUsearea', $findOne['UseArea']);
+                                Db::name('house')->where('HouseID', 'eq', $findOne['HouseID'])->setDec('LeasedArea', $findOne['LeasedArea']);
+                            } elseif ($findOne['RoomPublicStatus'] == 2) {    //两户共用的房屋
+                                $findHouses = explode(',', $findOne['HouseID']);
+                                Db::name('house')->where('HouseID', 'in', $findHouses)->setDec('HouseUsearea', $findOne['UseArea']);
+                                Db::name('house')->where('HouseID', 'in', $findHouses)->setDec('LeasedArea', $findOne['LeasedArea']);
+                            } elseif ($findOne['RoomPublicStatus'] > 2) {
+                                Db::name('house')->where('HouseID', 'in', $houses)->setDec('PublicRent', 0.5);
+                            }
+                            //将新面积数据加上去，如果是删除，就不需要这一步
+                            if ($datas['RoomPublicStatus'] == 1) {
+                                //添加，则直接将房间的使用面积、计租面积加到绑定的房屋中
+                                Db::name('house')->where('HouseID', 'eq', $datas['HouseID'])->setInc('HouseUsearea', $datas['UseArea']);
+                                Db::name('house')->where('HouseID', 'eq', $datas['HouseID'])->setInc('LeasedArea', $datas['LeasedArea']);
+                            } elseif ($datas['RoomPublicStatus'] == 2) {
+                                $datas['LeasedArea'] = $datas['LeasedArea'] / 2;
+                                Db::name('house')->where('HouseID', 'in', $houses)->setInc('HouseUsearea', $datas['UseArea']);
+                                Db::name('house')->where('HouseID', 'in', $houses)->setInc('LeasedArea', $datas['LeasedArea']);
+                            } elseif ($datas['RoomPublicStatus'] > 2) {
+                                Db::name('house')->where('HouseID', 'in', $houses)->setInc('PublicRent', 0.5);
+                                $datas['LeasedArea'] = 0;
+                            }
+
+                            Db::name('room')->update($datas);
+
+                            $tempRent = count_room_rent($datas['RoomID']);
+                            
+                            Db::name('room')->where('RoomID', $datas['RoomID'])->setField('RoomRentMonth', $tempRent);
+                                
+                            if ($diffHouses) {
+                                foreach ($diffHouses as $diff) {
+                                    $houseTemps = count_house_rent($diff);
+                                    Db::name('house')->where('HouseID',$diff)->setField('ApprovedRent',$houseTemps);
+                                }
+                            }
+                        }   
+                        
+                    } else { //不存在，则为新增
+
+
+                        $maxid = Db::name('room')->max('RoomID');
+                        $datas['RoomID'] = $maxid + 1;
+                        $datas['Status'] = 1;
+                        $datas['RoomName'] = Db::name('room_type_point')->where('id',$datas['RoomType'])->value('RoomTypeName');
+                        $datas['CreateUserID'] = session('user_base_info.uid');
+                        $datas['CreateTime'] = time();
+                        if ($datas['RoomPublicStatus'] == 1) {
+                            Db::name('house')->where('HouseID', 'eq', $datas['HouseID'])->setInc('HouseUsearea', $datas['UseArea']);
+                            Db::name('house')->where('HouseID', 'eq', $datas['HouseID'])->setInc('LeasedArea', $datas['LeasedArea']);
+                        } elseif ($datas['RoomPublicStatus'] == 2) {
+                            $datas['LeasedArea'] = $datas['LeasedArea'] / 2;
+                            Db::name('house')->where('HouseID', 'in', $houses)->setInc('HouseUsearea', $datas['UseArea']);
+                            Db::name('house')->where('HouseID', 'in', $houses)->setInc('LeasedArea', $datas['LeasedArea']);
+                        } elseif ($datas['RoomPublicStatus'] > 2) {
+                            $datas['LeasedArea'] = 0;
+                            Db::name('house')->where('HouseID', 'in', $houses)->setInc('PublicRent', 0.5); //三户共用的加5毛钱
+                        }
+                        //halt($datas);
+                        Db::name('room')->insert($datas);
+                        $tempRent = count_room_rent($datas['RoomID']);
+                        Db::name('room')->where('RoomID', $datas['RoomID'])->update(['RoomRentMonth' => $tempRent]);
+                        foreach ($houses as $hous) {
+                            $houseTemp = count_house_rent($hous);
+                            Db::name('house')->where('HouseID',$hous)->setField('ApprovedRent',$houseTemp);
+                        }
+                    }
+                }
+            }
+            //halt(1);
+            /*正式开始处理房间信息集*/
+            $houseArr = [
+                'Status' => 0, //此阶段全部调整为1状态
+                'HouseID' => $data['AddRent']['HouseID'],
+                'WallpaperArea' => $data['AddRent']['RentWallpaper'],
+                'CeramicTileArea' => $data['AddRent']['RentCeramic'],
+                'BathtubNum' => $data['AddRent']['RentBath'],
+                'BasinNum' => $data['AddRent']['RentBasin'],
+                'BelowFiveNum' => $data['AddRent']['RentBelow'],
+                'MoreFiveNum' => $data['AddRent']['RentMore'],
+                'OwnerType' => $data['AddRent']['TypeF']['OwnerType'],
+                'HousePrerent' => $data['AddRent']['TypeF']['RPrice'],
+                'AnathorOwnerType' => $data['AddRent']['TypeS']['OwnerType'],
+                'AnathorHousePrerent' => $data['AddRent']['TypeS']['RPrice'],
+                'DiffRent' => $data['AddRent']['DiffRent'],
+                'ProtocolRent' => $data['AddRent']['ProtocolRent'],
+                'PumpCost' => $data['AddRent']['PumpPrice'],
+                'IfWater' => $data['AddRent']['RIfWater'],
+            ];
+          
+
+            $itemPrices = Db::name('room_item_point')->column('id,UnitPrice');
+            $houseArr['PlusRent'] = $houseArr['WallpaperArea'] * $itemPrices[1] + $houseArr['CeramicTileArea'] * $itemPrices[2] + $houseArr['BathtubNum'] * $itemPrices[3] + $houseArr['BasinNum'] * $itemPrices[4] + $houseArr['BelowFiveNum'] * $itemPrices[5] + $houseArr['MoreFiveNum'] * $itemPrices[6];
+
+            //halt($houseArr);
+            $res = Db::name('house')->update($houseArr);
+            $temp = count_house_rent($data['AddRent']['HouseID']);
+            $areas = count_house_area($data['AddRent']['HouseID']);
+            //halt($temp);
+            Db::name('house')->where('HouseID', $data['AddRent']['HouseID'])->update(['ApprovedRent'=>$temp,'HouseUsearea'=>$areas['HouseUsearea'],'LeasedArea'=>$areas['LeaseArea']]);
+            //Db::name('system_tag')->where('TagType','RentSubmitTag')->setField('Tag',1);
+            return ($res !== false) ? jsons('2000', '操作成功') : jsons('4000', '操作失败');
+        }
+    }
+
+    /**
+     * 计租表直接修改【前期】
+     */
+    public function renttableOld()
+    {
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+            //halt($data);
+            //验证数据合法性
+            /*如果有未确认状态的要删除的房间则，再次删除*/
+            if (isset($data['AddRent']['deleteRoom'])) {
+                foreach ($data['AddRent']['deleteRoom'] as $v) {
+                    //先解绑之前的房屋，楼栋，并回滚面积
+                    $finds = Db::name('room')->field('HouseID,BanID,UseArea,LeasedArea,RoomPublicStatus')->where('RoomID', $v)->find();
+                    //回滚房屋使用面积,房间编号集
+                    if ($finds['RoomPublicStatus'] == 1) {    //私有房屋
+                        Db::name('house')->where('HouseID', 'eq', $finds['HouseID'])->setDec('HouseUsearea', $finds['UseArea']);
+                        Db::name('house')->where('HouseID', 'eq', $finds['HouseID'])->setDec('LeasedArea', $finds['LeasedArea']);
+                    } elseif ($finds['RoomPublicStatus'] == 2) {    //两户共用的房屋
+                        $findHouse = explode(',', $finds['HouseID']);
+                        Db::name('house')->where('HouseID', 'in', $findHouse)->setDec('HouseUsearea', $finds['UseArea'] / 2);
+                        Db::name('house')->where('HouseID', 'in', $findHouse)->setDec('LeasedArea', $finds['LeasedArea'] / 2);
+                    }
+                }
+                Db::name('room')->where('RoomID', 'in', $data['AddRent']['deleteRoom'])->delete();
+            }
+
+            if (isset($data['RoomType'])) {
+                $roomArr = model('ph/HouseInfo')->check_rent_table($data);
+                //halt($roomArr);
+                foreach ($roomArr as $k5 => $v5) {
+                    if (isset($data['PriceBox' . ($k5 + 1)])) { //写入基价折减的条件，id用逗号分隔
+                        $rentPointIDS = $data['PriceBox' . ($k5 + 1)];
+                    }
+                    /*  $v5的示例
+                    array(12){
+                        [0] =&gt; string(6) "120942"       房间编号
+                        [1] =&gt; string(2) "10"           间号
+                        [2] =&gt; string(10) "1010022266"       绑定的楼栋编号
+                        [3] =&gt; string(14) "10100222667294"   绑定的第一个房屋编号
+                        [4] =&gt; string(0) ""              绑定的第二个房屋编号
+                        [5] =&gt; string(0) ""              绑定的第三个房屋编号
+                        [6] =&gt; string(0) ""              绑定的第四个房屋编号
+                        [7] =&gt; string(0) ""              绑定的第五个房屋编号
+                        [8] =&gt; string(1) "2"             产别
+                        [9] =&gt; string(1) "2"             单元号
+                        [10] =&gt; string(1) "2"            楼层号
+                        [11] =&gt; string(5) "25.00"        使用面积
+                        [12] =&gt; string(3) "0.05"          `基价折减率
+                        [13] =&gt; string(3) "22"         房间的规定租金
+                        [14] =&gt; string(1) "0"            状态值0为正常，1为点击垃圾桶
+                        [15] =&gt; int(1)                   该房间的房间类型
+                    }
+                    */
+
+                    //如果重新绑定了楼栋，则重新将，机构，楼栋地址，产别付给房间
+                    $institutionArr = Db::name('ban')->field('InstitutionID,TubulationID,OwnerType,BanAddress')->where('BanID', $v5[2])->find();
+                    $houses = array_filter(array($v5[3], $v5[4], $v5[5],$v5[6],$v5[7])); //将传入的房屋编号放入数组并去空
+                    $diffHouses = array_merge(array_diff($houses, array($data['AddRent']['HouseID'])));
+                    $one = Db::name('room_type_point')->where('id', $v5[15])->field('RoomTypeName,Point')->find();
+                    $datas = [
+                        'RoomID' => $v5[0],
+                        'InstitutionID' => $institutionArr['TubulationID'],
+                        'InstitutionPID' => $institutionArr['InstitutionID'],
+                        'OwnerType' => $v5[8],
+                        'BanAddress' => $institutionArr['BanAddress'],
+                        'RoomNumber' => $v5[1],
+                        'BanID' => $v5[2],
+                        'HouseID' => implode(',', $houses),
+                        'RoomPublicStatus' => count($houses),
+                        'UnitID' => (int)$v5[9],
+                        'FloorID' => (int)$v5[10],
+                        'UseArea' => $v5[11],
+                        'RentPoint' => $v5[12],
+                        'RoomType' => $v5[15],
+                        'RoomPrerent' => $v5[13],
+                        'LeasedArea' => $v5[11] * $one['Point'],
+                        'RoomName' => $one['RoomTypeName'],
+                        'RentPointIDS' => $rentPointIDS,
+                    ];
                     if ($datas['RoomID']) {  //如果房间编号存在，则为修改
                         $datas['UpdateTime'] = time();
                         
