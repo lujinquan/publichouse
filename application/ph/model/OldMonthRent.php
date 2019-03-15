43fb5b7310af1be8240c0c6c358e0b1969000180
Lucas <18674012767@163.com>
2019-03-15 16:35:30 +0800

    sd

--- application/ph/model/OldMonthRent.php
+++ application/ph/model/OldMonthRent.php
@@ -77,7 +77,7 @@ class OldMonthRent extends Model
 
         //$where = isset($where)?$where:1;
 
-        $where['PayMonth'] = array('>',date('Y').'00');
+        $where['PayYear'] = array('eq',date('Y'));
 
         $fields = '*';
         $OldRentList['obj'] = self::field($fields)->where($where)->order('CreateTime desc')->paginate(config('paginate.list_rows'));
