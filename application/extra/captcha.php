fdd92c585d926ba577d267ec17b35f55043bf065
lu <992924033@qq.com>
2019-06-20 14:37:18 +0800

    配置

--- application/extra/captcha.php
+++ application/extra/captcha.php
@@ -10,24 +10,21 @@
 // +----------------------------------------------------------------------
 
 return [
-    //验证码配置
-    'captcha'  => [
-        //验证码字符
-        //'codeSet' => '2345678abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
-        'codeSet' => '2345678',
-        // 验证码字体大小(px)
-        'fontSize' => 18,
-        // 是否话混淆曲线
-        'useCurve'  => false,
-        // 是否添加杂点
-        'useNoise' => false,
-        //验证码图片高度
-        'imageH'  => 37,
-        //验证码图片宽度
-        'imageW'  =>  140,
-        // 验证码长度（位数）
-        'length'   => 4,
-        // 验证成功后是否重置
-        'reset'   =>  true
-    ],
+    //验证码字符
+    //'codeSet' => '2345678abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
+    'codeSet' => '2345678',
+    // 验证码字体大小(px)
+    'fontSize' => 18,
+    // 是否话混淆曲线
+    'useCurve'  => false,
+    // 是否添加杂点
+    'useNoise' => false,
+    //验证码图片高度
+    'imageH'  => 37,
+    //验证码图片宽度
+    'imageW'  =>  140,
+    // 验证码长度（位数）
+    'length'   => 4,
+    // 验证成功后是否重置
+    'reset'   =>  true    
 ];
